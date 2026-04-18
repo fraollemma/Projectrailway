6985ee3b7431dfa5c16778197ed5868175b7b471
Fraol Lemma <fraollemma@example.com>
2026-04-18 21:41:19 +0300

    16

--- users/models.py
+++ users/models.py
@@ -100,4 +100,4 @@ class Profile(models.Model):
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.get_or_create(user=instance)
