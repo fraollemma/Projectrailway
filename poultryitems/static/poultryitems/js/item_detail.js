b469e92ab5d8a3c62a8b9867b66ff5a6c0f9d658
Fraol Lemma <fraollemma@example.com>
2026-04-23 21:58:42 +0300

    test 145

--- poultryitems/static/poultryitems/js/item_detail.js
+++ poultryitems/static/poultryitems/js/item_detail.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const likeButtons = document.querySelectorAll('[data-action="like"]');
   const shareButtons = document.querySelectorAll('[data-action="share"]');
-
+  const mainImage = document.getElementById("mainImage");
+  const thumbnails = document.querySelectorAll(".thumbnail");
   likeButtons.forEach(btn => btn.addEventListener('click', handleLike));
   shareButtons.forEach(btn => btn.addEventListener('click', handleShare));
+  thumbnails.forEach(thumb => {
+        thumb.addEventListener("click", function () {
+            // Swap image
+            mainImage.src = this.src;
 
+            // Optional: active border effect
+            thumbnails.forEach(t => t.classList.remove("active"));
+            this.classList.add("active");
+        })
+  });
   async function handleLike(e) {
     const btn = e.currentTarget;
     const itemId = btn.dataset.itemId;
