0bd8fb12afffa03a23c046a57e64cbe270479304
Fraol Lemma <fraollemma@example.com>
2026-04-26 02:32:11 +0300

    fix 172

--- base/static/base/js/base.js
+++ base/static/base/js/base.js
@@ -274,18 +274,17 @@ function initDropdowns() {
 
         const toggle = dropdown.querySelector(".dropdown-toggle");
 
-        toggle.addEventListener("click", function(e) {
+        if (toggle) { 
+            toggle.addEventListener("click", function(e) {
+                e.stopPropagation();
 
-            e.stopPropagation();
+                dropdowns.forEach(d => {
+                    if (d !== dropdown) d.classList.remove("open");
+                });
 
-            dropdowns.forEach(d => {
-                if (d !== dropdown) d.classList.remove("open");
+                dropdown.classList.toggle("open");
             });
-
-            dropdown.classList.toggle("open");
-
-        });
-
+        }
     });
 
     document.addEventListener("click", function() {
@@ -415,3 +414,27 @@ function addPulseAnimation() {
 }
 
 addPulseAnimation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
