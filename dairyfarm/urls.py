3d9e3b232b8e388aaee67c5e78f6faec46ecefe1
Fraol Lemma <fraollemma@example.com>
2026-04-30 12:59:33 +0300

    error 4

--- dairyfarm/urls.py
+++ dairyfarm/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "dairyfarm"
 
 urlpatterns = [
-    path("", views.DairyProductListView.as_view(), name="product_list"),
-    path("add/", views.DairyProductCreateView.as_view(), name="product_add"),
-    path("<slug:slug>/", views.DairyProductDetailView.as_view(), name="product_detail"),
+    path("", views.DairyProductListView.as_view(), name="dairyfarm_list"),
+    path("add/", views.DairyProductCreateView.as_view(), name="dairyfarm_add"),
+    path("<slug:slug>/", views.DairyProductDetailView.as_view(), name="dairyfarm_detail"),
     path("category/<slug:slug>/", views.DairyCategoryView.as_view(), name="category"),
     path("<slug:slug>/order/", views.CreateOrderView.as_view(), name="order"),
 ]
