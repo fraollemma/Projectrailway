7eaf1344d0f860788261eb19be8cc38db80dda3a
Fraol Lemma <fraollemma@example.com>
2026-05-01 19:54:06 +0300

    21web

--- cart/urls.py
+++ cart/urls.py
@@ -1,3 +1,4 @@
+# cart/urls.py
 from django.urls import path
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path("", views.cart_detail, name="cart_detail"),
     path("add/<str:app_label>/<str:model_name>/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
     path("remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
+    path("toggle/<slug:slug>/", views.toggle_cart_item, name="toggle_cart_item"),
 ]
