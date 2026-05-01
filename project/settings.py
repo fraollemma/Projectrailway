960a24722bd30d1e76bbe82745e930a8efec12b1
Fraol Lemma <fraollemma@example.com>
2026-05-01 03:54:35 +0300

    10fraollemma

--- project/settings.py
+++ project/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
  
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
